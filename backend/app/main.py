8a73daf57905019b4341b19ba740fb43bcbc16b1
Mahesh <kagithacm237@gmail.com>
2026-07-24 09:25:11 +0530

    Initial FastAPI and PostgreSQL setup

---
+++ backend/app/main.py
@@ -0,0 +1,36 @@
+# from fastapi import FastAPI
+
+# from app.api.v1.health import router as health_router
+
+# from app.core.config import settings
+
+
+# app = FastAPI(
+#     title=settings.APP_NAME,
+#     version="1.0.0"
+# )
+
+# app.include_router(
+#     health_router,
+#     prefix="/api/v1",
+#     tags=["Health"]
+# )
+
+from fastapi import FastAPI
+from app.api.v1.health import router as health_router
+from app.core.config import settings
+
+app = FastAPI()
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version="1.0.0"
+)
+app.include_router(
+    health_router,
+    prefix="/api/v1",
+    tags=["Health"]
+)
+@app.get("/")
+def root():
+    return {"message": "Hello Crop Doctor AI"}
